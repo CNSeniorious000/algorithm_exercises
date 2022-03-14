4f382b0d30475eb03cd40a054f7ee0e1d3ec4440
MuspiMerol <wjj123345567@iCloud.com>
2022-03-14 13:46:22 +0800

    add test

--- U204635/solutions/__init__.py
+++ U204635/solutions/__init__.py
@@ -5,5 +5,20 @@ def generate_testcase(n):
     raise NotImplementedError
 
 
+example_testcase = (5, [
+    [0, 3, 6, -1, 1],
+    [3, 0, 2, -1, 4],
+    [6, 2, 0, 2, 3],
+    [-1, -1, 2, 0, 1],
+    [1, 4, 3, 1, 0]
+])
+
 if __name__ == '__main__':
-    pass
+    from wyh_solution import solve
+    assert solve(*example_testcase) == 6
+
+    from BF_solution import solve
+    assert solve(*example_testcase) == 6
+
+    from DP_solution import solve
+    assert solve(*example_testcase) == 6
