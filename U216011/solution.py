337b6b437a0b321ddd68ead6c654cdb0dc2880a5
MuspiMerol <wjj123345567@iCloud.com>
2022-05-06 02:26:54 +0800

    perfect and must be right

---
+++ U216011/solution.py
@@ -0,0 +1,18 @@
+def solve(n, matrix, v):
+    ans = matrix[v].copy()
+    to_scan = set(range(n)) - {v}
+    for _ in range(n - 1):
+        to_scan.remove(index := min([i for i in to_scan if ~ans[i]], key=lambda x: ans[x]))
+        total = ans[index]
+        row = matrix[index]
+        for i in to_scan:
+            if ~row[i] and ((dist := row[i] + total) < ans[i] or ans[i] == -1):
+                ans[i] = dist
+
+    return ans[:v] + ans[v + 1:]
+
+
+if __name__ == '__main__':
+    print(*map(str, solve(lth := next(numbers := map(int, input().split())),
+                          [[next(numbers) for _ in range(lth)] for _ in range(lth)],
+                          next(numbers))), end="")
